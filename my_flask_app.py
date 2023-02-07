c299c88f66c7835be02ef86de1e2667e02e71868
Noah Lopez <loalno12@gmail.com>
2023-02-06 20:43:39 -0800

    updated minecraft page to look good and show server status

--- my_flask_app.py
+++ my_flask_app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import logging
 import datetime
+import socket
 
 app = Flask(__name__)
 debug = False
@@ -14,14 +15,26 @@ def home():
         email = request.form['Email']
         message = request.form['Message']
         with open(submission_filename, 'a') as submission_file:
-            submission_file.write('\n'.join([name,email,message])+'\n')
-            submission_file.write('\n')
+            submission_file.write(':'.join([name,email,message])+'\n')
         
     return render_template('main.html', user_ip=request.remote_addr)
 
 @app.route("/minecraft")
 def minecraft_page():
-    return render_template('minecraft_page.html')
-    
+    if is_server_down('mc.kylegay.com', 25565):
+        mc_server_status = "Server is currently down"
+    else:
+        mc_server_status = "Server is currently up"
+    return render_template('minecraft_page.html', mc_server_status=mc_server_status)
+
+import socket
+
+def is_server_down(host, port):
+    try:
+        s = socket.create_connection((host, port), timeout=2)
+        return False
+    except socket.error as e:
+        return True
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
