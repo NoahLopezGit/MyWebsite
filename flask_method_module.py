ac710d0205280513818bf43066206bcf98d7b744
Noah Lopez <loalno12@gmail.com>
2023-02-11 13:55:25 -0800

    change

--- flask_method_module.py
+++ flask_method_module.py
@@ -2,7 +2,12 @@ import os
 import openai
 import json
 
-openai.api_key = json.load(open('/var/www/html/MyWebsite/openai_api_key.json'))['OPENAI_API_KEY']
+debug=False
+
+if debug==False:
+    openai.api_key = json.load(open('/var/www/html/MyWebsite/openai_api_key.json'))['OPENAI_API_KEY']
+else:
+    openai.api_key = json.load(open('openai_api_key.json'))['OPENAI_API_KEY']
 
 def get_welcome_message(name):
     response = openai.Completion.create(
